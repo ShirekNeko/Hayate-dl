ad82fbebc0bdd0d5cf77a6b234f5b824f4f7f4d6
Rafał Kłusek <40665869+ShirekNeko@users.noreply.github.com>
2021-08-20 20:37:55 +0200

    hayate-dl v0.9.1
    Poprawa kodu, aktualizacja bibliotek. usprawniona wersja przy ściąganiu stron mangi

--- hayate-dl.js
+++ hayate-dl.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         hayate-dl
 // @namespace    http://tampermonkey.net/
-// @version      0.9
+// @version      0.9.1
 // @description  Pobieranie mangi ze strony hayate.eu
 // @author       Shiro
 // @match        https://reader.hayate.eu/*
 // @match        https://hayate.eu/*
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @require      https://cdn.jsdelivr.net/npm/js-base64@3.6.0/base64.min.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.0/FileSaver.min.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.3.2/html2canvas.min.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_deleteValue
@@ -17,8 +17,8 @@
 // @run-at       document-start
 // ==/UserScript==
 
-//funkcja nadajaca styl
-GM_addStyle ( `
+// funkcja nadajaca styl
+GM_addStyle(`
     .btn_script{
         position: relative;
         display: block;
@@ -43,84 +43,84 @@ GM_addStyle ( `
         top:  0px;
         width: 200px;
     }
-` );
-//funkcja zwraca hash strony, tj #cyfra
-function getNumerPage(){
-    let url_regex = /^(https|http):\/\/(.+)\/*$/;
-	let url_path = window.location.hash;
+`);
+// funkcja zwraca hash strony, tj #cyfra
+function getNumerPage() {
+    let url_path = window.location.hash;
     return url_path;
 }
 
-//funkcja zwraca nazwę projektu
-function getMangaName(){
-    let url_regex = /^(https|http):\/\/(.+)\/*$/;
-	let url_path = window.location.pathname;
-	var mangaData = url_path.split("/");
+// funkcja zwraca nazwę projektu
+function getMangaName() {
+    let url_path = window.location.pathname;
+    var mangaData = url_path.split("/");
     return mangaData;
 }
 
-//funkcja zwraca nazwę hosta
-function getMangaHostName(){
-    let url_regex = /^(https|http):\/\/(.+)\/*$/;
-	let url_path = window.location.hostname;
+// funkcja zwraca nazwę hosta
+function getMangaHostName() {
+    let url_path = window.location.hostname;
     var mangaURL = url_path.split("/");
     return mangaURL;
 }
 
-//funkcja wywołana na readerze dodająca przycisk pobierajacy całą mangę
-function handleMangaAllPage(){
+// funkcja wywołana na readerze dodająca przycisk pobierajacy całą mangę
+function handleMangaAllPage() {
     var btnPlace = $(".single").first();
     var btnCode = "<button id='All' class='btn_script' style='margin-bottom: 15px;'>Pobierz całą mangę</button>";
     btnPlace.html(btnPlace.html() + btnCode);
 
-    $("#All").on("click", function() {
+    $("#All").on("click", function () {
         var mangaName = getMangaName();
-        var saveProject = [mangaName[2],"true"];
-        GM_setValue("project",saveProject);
-        location.href = "https://reader.hayate.eu/" + mangaName[2]+ '/1/1/view#1';
+        var saveProject = [mangaName[2], "true"];
+        GM_setValue("project", saveProject);
+        location.href = "https://reader.hayate.eu/" + mangaName[2] + '/1/1/view#1';
     });
 }
-// przyszła funkcjonalność - 
-function handleMangaChapterPage(){
+
+// przyszła funkcjonalność -
+function handleMangaChapterPage() {
     var btnPlace = $(".single").first();
     var btnCode = "<button id='Chapter' class='btn_script' style='margin-bottom: 15px; z-position: 12;'>Pobierz rozdział mangi</button>";
     btnPlace.html(btnPlace.html() + btnCode);
 }
 
-//Funkcja, która ściąga karty mangi
-function downloaderManga(){
+// Funkcja, która ściąga karty mangi
+function downloaderManga() {
     var mangaPathName = getMangaName();
     var currentPage = getNumerPage();
-    setTimeout(function() {
+    setTimeout(function () {
         let element = document.querySelector('.page');
-        html2canvas(element, {
-            onrendered: function(canvas) {
-                var imageData = canvas.toDataURL("image/png");
-                window.saveAs(imageData, mangaPathName[1] +`_vol` + mangaPathName[2] + `chap` + mangaPathName[3] + `_page-${currentPage[1]}.png`);
-            }
+        html2canvas(element).then(canvas => {
+            var imageData = canvas.toDataURL("image/png");
+            window.saveAs(imageData, mangaPathName[1] + `_vol` + mangaPathName[2] + `chap` + mangaPathName[3] + `_page-${
+                currentPage[1]
+            }.png`);
         });
         var i = currentPage[1];
         i++;
-        alert(i);
-        var urf ="https://reader.hayate.eu/" + mangaPathName[1]+ '/' + mangaPathName[2]+ '/' + mangaPathName[3]+ '/view#' + i;
+        var urf = "https://reader.hayate.eu/" + mangaPathName[1] + '/' + mangaPathName[2] + '/' + mangaPathName[3] + '/view#' + i;
         location.href = urf;
-    }, 1000);
-    setTimeout(function() {
+
+    }, 3000);
+    setTimeout(function () {
         downloaderManga();
-    }, 1000);
+    }, 3000);
+
 }
 
-//funkcja główna
-(function() {
+// funkcja główna
+(function () {
     'use strict';
 
     handleMangaAllPage();
-   // handleMangaChapterPage();
+    // handleMangaChapterPage();
     var mangaPathName = getMangaName();
     var mangaHostName = getMangaHostName();
     var mangaIf = GM_getValue("project");
-    if(mangaHostName == "reader.hayate.eu" && mangaIf[1] == "true" && mangaIf[0] == mangaPathName[1]){
-        downloaderManga(mangaPathName);
-   }
+    if (mangaHostName == "reader.hayate.eu" && mangaIf[1] == "true" && mangaIf[0] == mangaPathName[1]) {
+        alert(mangaIf[0]);
+        downloaderManga();
+    }
 
 })();
